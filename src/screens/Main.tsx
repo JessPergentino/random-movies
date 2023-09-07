e28bec68c73c09ae1824f635c3f2384861d77640
Jessica Pergentino <jessicapergentino@hotmail.com>
2023-09-07 15:38:12 -0300

    Initial commit

---
+++ src/screens/Main.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+function Main(): JSX.Element {
+  return (
+    <View>
+      <Text>Streams</Text>
+      <Text>Streams</Text>
+      <Text>Streams</Text>
+      <Text>Streams</Text>
+    </View>
+  );
+}
+export default Main;
